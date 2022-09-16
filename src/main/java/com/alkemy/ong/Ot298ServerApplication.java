40add25c74cd706380eac1b9104e43b07f5cb192
Alejandro <alj70111@gmail.com>
2022-09-16 15:26:31 -0300

    Initial Commint

---
+++ src/main/java/com/alkemy/ong/Ot298ServerApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ot298ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ot298ServerApplication.class, args);
+	}
+
+}
